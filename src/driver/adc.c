c65896db57359dea70e17fefe01a3ab9469535d4
bkleiner <b.kleiner@str8labs.com>
2023-03-21 02:09:41 +0100

    target: add ibat, vbat, usb_detect and fpv

--- src/driver/adc.c
+++ src/driver/adc.c
@@ -38,7 +38,7 @@ typedef struct {
 #endif
 
 #define VREFINT_CAL (*(VREFINT_CAL_ADDR))
-#define VBAT_SCALE ((float)(VBAT_DIVIDER_R1 + VBAT_DIVIDER_R2) / (float)(VBAT_DIVIDER_R2) * (1.f / 1000.f))
+#define VBAT_SCALE (profile.voltage.vbat_scale * (1.f / 10000.f))
 
 #ifdef STM32H7
 static const adc_dev_t adc_dev[ADC_DEVICE_MAX] = {
@@ -231,12 +231,12 @@ void adc_init() {
 
   adc_init_pin(ADC_CHAN_VREF, PIN_NONE);
   adc_init_pin(ADC_CHAN_TEMP, PIN_NONE);
-#ifdef VBAT_PIN
-  adc_init_pin(ADC_CHAN_VBAT, VBAT_PIN);
-#endif
-#ifdef IBAT_PIN
-  adc_init_pin(ADC_CHAN_IBAT, IBAT_PIN);
-#endif
+  if (target.vbat != PIN_NONE) {
+    adc_init_pin(ADC_CHAN_VBAT, target.vbat);
+  }
+  if (target.ibat != PIN_NONE) {
+    adc_init_pin(ADC_CHAN_IBAT, target.ibat);
+  }
 
   for (uint32_t i = 0; i < ADC_DEVICE_MAX; i++) {
     adc_init_dev(i);
@@ -278,21 +278,19 @@ float adc_read(adc_chan_t chan) {
   }
 
   case ADC_CHAN_VBAT:
-#ifdef VBAT_PIN
+    if (target.vbat == PIN_NONE) {
+      return 4.20f;
+    }
     return adc_convert_to_mv(adc_read_raw(chan)) * VBAT_SCALE * (profile.voltage.actual_battery_voltage / profile.voltage.reported_telemetry_voltage);
-#else
-    return 4.20f;
-#endif
 
   case ADC_CHAN_IBAT:
-#ifdef IBAT_PIN
     if (profile.voltage.ibat_scale == 0) {
       return 0;
     }
+    if (target.ibat == PIN_NONE) {
+      return 0;
+    }
     return adc_convert_to_mv(adc_read_raw(chan)) * (10000.0f / profile.voltage.ibat_scale);
-#else
-    return 0;
-#endif
 
   default:
     return adc_read_raw(chan);
